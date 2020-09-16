5fe79bff80eaff02813c6b325b1a1fbb1b5a02b1
km <35552602+catnap7777@users.noreply.github.com>
2020-09-15 20:44:21 -0500

    Initial Commit

---
+++ MAD157Module5FamilyTreeKarenM/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MAD157Module5FamilyTreeKarenM
+//
+//  Created by Karen Mathes on 9/15/20.
+//  Copyright © 2020 TygerMatrix Software. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
