4a50c8dee243952def5246f945a3b343d667d6cf
km <35552602+catnap7777@users.noreply.github.com>
2020-09-15 23:34:34 -0500

    added nav controller

--- MAD157Module5FamilyTreeKarenM/FirstViewController.swift
+++ MAD157Module5FamilyTreeKarenM/FirstViewController.swift
@@ -15,6 +15,8 @@ class FirstViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonClicked(_ sender: Any) {
+    }
+    
 }
 
